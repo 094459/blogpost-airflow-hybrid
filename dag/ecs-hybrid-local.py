c00fb1a7bdc68d0127ddbf6e1c65b71d8a01f0d0
ricsue <ricsue@amazon.com>
2022-03-07 11:18:12 +0000

    new DAG

---
+++ dag/ecs-hybrid-local.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.providers.amazon.aws.operators.ecs import ECSOperator
+
+
+default_args = {
+    'owner': 'ubuntu',
+    'start_date': datetime(2019, 8, 14),
+    'retry_delay': timedelta(seconds=60*60)
+}
+
+with DAG('hybrid_airflow_local_dag', catchup=False, default_args=default_args, schedule_interval=None) as dag:
+
+    localquery = ECSOperator(
+        task_id="localquery",
+        dag=dag,
+        cluster="hybrid-airflow-cluster",
+        task_definition="apache-airflow",
+        overrides={ "containerOverrides": [
+            { 
+                "name": "Hybrid-ELT-TaskDef",
+                "command" : [ "ricsue-airflow-hybrid","period1/region-data.csv", "select * from customers WHERE country = \"Spain\"", "mydc-airflow-hybrid","eu-west-2" ]} 
+            ] },
+        launch_type="EXTERNAL",
+        awslogs_group="/ecs/hybrid-airflow",
+        awslogs_stream_prefix="ecs/Hybrid-ELT-TaskDef",
+        reattach = True
+    )
+    
+    localquery
